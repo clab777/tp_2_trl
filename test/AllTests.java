77cf50940e81d9995dab6ea52c5b75993962d392
sleng0022 <leng.sinith001@gmail.com>
2018-04-17 22:56:14 -0500

    add more test

--- test/AllTests.java
+++ test/AllTests.java
@@ -3,7 +3,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ TestLibrary.class, test_book.class, test_fine.class})
+@SuiteClasses({ TestLibrary.class, test_book.class, test_fine.class, test_student.class, test_teacher.class})
 public class AllTests {
 
 }
